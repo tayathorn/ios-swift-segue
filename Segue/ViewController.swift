780a3fc9321a5fd32a871f96264c002f96c8be58
tayathorn <>
2018-07-16 12:15:10 +0700

    Create new app

---
+++ Segue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Segue
+//
+//  Created by EksPan on 16/7/2561 BE.
+//  Copyright © 2561 EksPan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
