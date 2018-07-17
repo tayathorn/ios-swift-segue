4fdc8c89b2c718879addbf51c6454d424061fca4
tayathorn <>
2018-07-17 19:35:23 +0700

    + Override Prepare For Segue

--- Segue/SecondViewController.swift
+++ Segue/SecondViewController.swift
@@ -10,10 +10,14 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    var message : String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        messageLabel.text = message!
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +25,6 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
--- Segue/ViewController.swift
+++ Segue/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Check segue identifier
+        if segue.identifier == "goToSecondViewController" {
+            // Get SecondVC
+            let destinationVC = segue.destination as! SecondViewController
+            
+            // Pass text to SecondVC
+            destinationVC.message = textField.text!
+        }
+    }
 }
 
